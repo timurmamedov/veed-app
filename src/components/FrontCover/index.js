fe11856b40bca4eb9e361ef04d6e102de6e744b4
Timur Mamedov <timurmamedov@protonmail.com>
2017-02-21 21:35:32 +0000

    Further frontend improvement. Add banner, logo, magazines. TODO FrontCover component needs css improvement.

--- src/components/FrontCover/index.js
+++ src/components/FrontCover/index.js
@@ -1,46 +1,66 @@
 import React, { PropTypes, Component } from 'react';
 import classnames                      from 'classnames';
+import VideoItem                       from '../VideoItem';
 
 import './style.css';
-import testVideo from './hello.mp4';
-import Video     from 'react-html5video';
+
+import logo       from './logo.png';
+import iphone     from './iphone.png';
+import magazine0  from './magazine0.png';
+import magazine1  from './magazine1.png';
+import magazine2  from './magazine2.png';
+
+import Video      from 'react-html5video';
+import promoVideo from './promoVideo.mp4';
 
 export default class FrontCover extends Component {
   // static propTypes = {}
   // static defaultProps = {}
   // state = {}
 
-  componentDidMount() {
-    setTimeout(function(){ 
-      // pause video 1, do 0. video 2, do 1. simple
-      // document.getElementsByTagName('video')[0].pause(); 
-    }, 3000);
-    console.log(document.getElementsByTagName('video'));
-  }
-
   render() {
     const { className, ...props } = this.props;
+    const videoItemName = 'VOGUE';
     return (
       <div className={classnames('FrontCover', className)} {...props}>
         <div className="App-header">
-          <h1>VEED</h1>
-          
-            <div className="Video-wrapper">
-              <div className="inner">
-                <div className="video-title">
-                  <div className="video-font"> HYPEBEAST </div>
+
+          <div className="banner">
+            <div className="bannerBox">
+              <div className="bannerBox-left">
+                <div className="logoSquare-top">
+                  <img className="logoImg" src={logo} />
                 </div>
-                <Video width="400" height="400" autoPlay loop muted
-                    // poster="http://sourceposter.jpg"
-                    onCanPlayThrough={() => {
-                        // Do stuff
-                    }}>
-                    <source src={testVideo} type="video/mp4" />
-                </Video>
-                
+                <div className="logoSquare-bottom">VEED’s content platform is the new way for marketeers and writers to create video for various markets, in a range of styles, for multiple channels at scale.</div>
               </div>
-              <div className="shadow"></div>
+              <div className="bannerBox-right floating">
+                <img className="iphoneImg" src={iphone} />
+                <Video className="promoVideo" autoPlay loop muted>
+                  <source src={promoVideo} type="video/mp4" />
+                </Video>
+              </div>
             </div>
+          </div>
+
+          <div className="banner">
+            <div className="bannerBox">
+              <div className="magazine-left">
+                <img className="magazine0Img" src={magazine0} />
+              </div>
+              <div className="magazine-middle">
+                <img className="magazine1Img" src={magazine1} />
+              </div>
+              <div className="magazine-right">
+                <img className="magazine2Img" src={magazine2} />
+              </div>
+            </div>
+          </div>
+
+          <VideoItem videoItemName={videoItemName}/>
+
+          <br /><br />
+
+          <VideoItem videoItemName={videoItemName}/>
 
         </div>
       </div>
---
+++ src/components/VideoItem/index.js
@@ -0,0 +1,44 @@
+import React, { PropTypes, Component } from 'react';
+import classnames                      from 'classnames';
+
+import './style.css';
+import testVideo from './hello.mp4';
+import Video     from 'react-html5video';
+
+export default class VideoItem extends Component {
+  // static propTypes = {}
+  // static defaultProps = {}
+  // state = {}
+
+  componentDidMount() {
+    setTimeout(function(){ 
+      // pause video 1, do 0. video 2, do 1. simple
+      // document.getElementsByTagName('video')[0].pause(); 
+    }, 3000);
+    console.log(document.getElementsByTagName('video'));
+  }
+
+
+  render() {
+    const { className, ...props } = this.props;
+    return (
+      <div className={classnames('VideoItem', className)} {...props}>
+
+          <div className="inner">
+            <div className="video-title">
+              <div className="video-font"> {this.props.videoItemName} </div>
+            </div>
+            <Video className="squareVideo" width="400" height="400" autoPlay loop muted
+                // poster="http://sourceposter.jpg"
+                onCanPlayThrough={() => {
+                    // Do stuff
+                }}>
+                <source src={testVideo} type="video/mp4" />
+            </Video>
+          </div>
+          <div className="shadow"></div>
+
+      </div>
+    );
+  }
+}
